f05d63aa751742eab9e5dbf76c27e873b10e9b86
vincethewalker <vincethewalker@yahoo.co.uk>
2020-08-28 17:04:54 +0100

    Gem Updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181024124644) do
+ActiveRecord::Schema.define(version: 2018_10_24_124644) do
 
   create_table "topics", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "picture"
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
 end
